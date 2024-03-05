5f16fa1c06e35d775d87bd66babf340cf4a99194
Alex <lesha.rudiko@gmail.com>
2024-03-05 16:51:39 +0300

    update

--- main.js
+++ main.js
@@ -13,56 +13,32 @@ const editingPage = () => {
 }
 
 const changeBookPlace = () => {
+    const list = books[2].querySelector('ul')
+    const chapters = list.querySelectorAll('li')
+
     books[1].after(books[0])
     books[4].after(books[3])
     bookBlock.append(books[2])
+
+    chapters[8].insertAdjacentHTML('afterend', '<li>Глава 8: За пределами ES6</li>')
 }
 
-const chapterOrder = () => {
-    const liOrder2 = books[0].querySelectorAll('ul > li')
-    const liOrder5 = books[5].querySelectorAll('ul > li')
-    const liOrder6 = books[2].querySelectorAll('ul > li')
-    const listUl5 = books[5].querySelector('ul')
-    const listUl2 = books[0].querySelector('ul')
+const chapterOrder = (book, newOrder) => {
+    const list = book.querySelector('ul')
+    const chapters = list.querySelectorAll('li')
+    const fragment = document.createDocumentFragment()
 
-    const fragmentOrder2 = document.createDocumentFragment()
-    const fragmentOrder5 = document.createDocumentFragment()
+    newOrder.forEach((index) => fragment.append(chapters[index]))
 
-    fragmentOrder2.append(
-        liOrder2[0],
-        liOrder2[1],
-        liOrder2[3],
-        liOrder2[6],
-        liOrder2[8],
-        liOrder2[4],
-        liOrder2[5],
-        liOrder2[7],
-        liOrder2[9],
-        liOrder2[2],
-        liOrder2[10],
-        )
-
-    fragmentOrder5.append(
-        liOrder5[0],
-        liOrder5[1],
-        liOrder5[9],
-        liOrder5[3],
-        liOrder5[4],
-        liOrder5[2],
-        liOrder5[6],
-        liOrder5[7],
-        liOrder5[5],
-        liOrder5[8],
-        liOrder5[10],
-    )
-
-    listUl5.append(fragmentOrder5)
-    listUl2.append(fragmentOrder2)
-    liOrder6[8].insertAdjacentHTML('afterend', '<li>Глава 8: За пределами ES6</li>')
+    list.append(fragment)
 }
+
+const newBook2 = [0, 1, 3, 6, 8, 4, 5, 7, 9, 2, 10]
+const newBook5 = [0, 1, 9, 3, 4, 2, 6, 7, 5, 8, 10]
 
 editingPage()
 changeBookPlace()
-chapterOrder()
+chapterOrder(books[0], newBook2)
+chapterOrder(books[5], newBook5)
 
 
